2de109bc60403ec7aa27dcd8caeb1e67e39cf26c
himalkarkee000 <himalkarkee000@gmail.com>
2024-09-26 12:40:55 +0545

    image done

--- src/components/landingpage/banner.tsx
+++ src/components/landingpage/banner.tsx
@@ -1,32 +1,45 @@
-import "./banner.css"
-import Image from "next/image"
+import "./banner.css";
+import Image from "next/image";
 const Banner = () => {
   return (
-    <div className="container h-[550] w-full bg-cover bg-center relative">
-        <Image src="/vech1.webp" alt="first" 
-            className="w-full h-[80vh]"
-            width={1000}
-            height={10000}/> 
-        <div className="container-1  mx-auto ">
-        <div className="grid gap-4 py-4 md:grid-cols-1">
-            <div className="mr-7 md:ps-20 lg:px-2">
-                <div className="mr-9 md:ps-20 lg:px-2 xl:px-2">
-                    <div className="mr-9 w-full">
-                    <h1 className="m-0 font-bold text-white ">Your dream vehilce</h1>
-                    <p className="text-white">Lorem</p>
-                    </div>
-                </div>
-                <div></div>
-            </div>   
-        </div>
-        </div>
-        
-        <div>
+    <>
+      <div className="mt-2 container  w-full bg-cover bg-center relative ">
+        <Image
+          src="/images/vech1.png"
+          alt="first"
+          className="w-full fill   "
+          width={1000}
+          height={10000}
+        />
+        <div className="container-a   ">
+          <div className="container-aa">
+            <div className="container-aaa relative ">
+             
+                <div className="mr-9 absolute top-1 right-0">
+                  <h1 className="m-0 ">
+                    Your dream vehilce
+                  </h1>
+                  <p className="">Is Just Few Miles Away</p>
+                
+              </div>
+              
+              
+            </div>
+            <div className="container-aab bg-green-200">
+                This is form container
 
-        </div>
-      
-    </div>
-  )
-}
+              </div>
 
-export default Banner
+          </div>
+        </div>
+      </div>
+
+
+      <div className="container2 bg-green-400">
+
+      </div>
+    </>
+  );
+};
+
+export default Banner;
