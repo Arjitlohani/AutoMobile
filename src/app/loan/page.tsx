b454cf8cfd8b266ec2331301a9ee296196cb63b0
Arjit <arjitloh223@gmail.com>
2024-10-30 16:40:12 +0545

    loan form

--- src/app/loan/page.tsx
+++ src/app/loan/page.tsx
@@ -1,10 +1,15 @@
+// Page.tsx
 'use client'
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import LoanNav from "@/components/utils/LoanNav";
 import Navbar from "@/components/utils/Navbar";
+import CustomInput from "@/components/common/CustomeInput";
+
 
 const Page = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [vehicleType, setVehicleType] = useState(false);
+
     const handleProfileCard = () => {
         console.log("Profile card handled");
     };
@@ -14,9 +19,27 @@ const Page = () => {
     const handleDropdownToggle = () => {
         setIsDropdownOpen((prev) => !prev);
     };
+    const handleVehicleType = () => {
+        setVehicleType((prev) => !prev);
+    };
+
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [error, setError] = useState('');
+
+    const handleInputChange = (setter: React.Dispatch<React.SetStateAction<string>>) => (e: ChangeEvent<HTMLInputElement>) => {
+        setter(e.target.value);
+        if (e.target.value) {
+            setError('');
+        } else {
+            setError('Please enter something');
+        }
+    };
 
     return (
-        <>
+        <div className="max-w-screen overflow-hidden">
             <Navbar handelProfileCard={handleProfileCard} isOpenProfileCard={isOpenProfileCard} />
             <LoanNav />
             <h3 className="ml-7">Home / Loan</h3>
@@ -24,9 +47,9 @@ const Page = () => {
                 <div className="w-full">
                     <button 
                         onClick={handleDropdownToggle} 
-                        className=" bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 
+                        className="bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 
                         font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600
-                         dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
+                        dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
                         type="button"
                     >
                         Why Millions Choose Droom for Vehicle Loan? 
@@ -54,51 +77,99 @@ const Page = () => {
                             </ul>
                         </div>
                     )}
-                    <div >
+                    <div>
                       <button className="flex bg-white items-center">
-                      <h3>Lending Partners at Droom</h3>
-                      <svg className="w-2.5 h-2.5 ms-3 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <h3>Lending Partners at Droom</h3>
+                        <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1L5 5L9 1" />
                         </svg>
-                        
                       </button>
                     </div>
                 </div>
 
-                <div className="">
+                <div className="w-full max-w-screen px-4">
                     <form className="w-full max-w-lg m-5 bg-white p-4 shadow-md">
+                        <h1 className="text-center text-[24px] m-3 text-gray-600">Apply for Loan & Take Your Vehicle Home</h1>
                         {/* Form fields */}
-                        {/* First Name and Last Name */}
                         <div className="flex flex-wrap -mx-3 mb-6">
                             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
-                                    First Name
-                                </label>
-                                <input 
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" 
-                                    id="grid-first-name" 
-                                    type="text" 
-                                    placeholder="Jane"
+                                <CustomInput 
+                                    id="firstname" 
+                                    label="First Name" 
+                                    value={firstname} 
+                                    onChange={handleInputChange(setFirstname)} 
+                                    error={error} 
                                 />
                             </div>
                             <div className="w-full md:w-1/2 px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
-                                    Last Name
-                                </label>
-                                <input 
-                                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
-                                    id="grid-last-name" 
-                                    type="text" 
-                                    placeholder="Doe"
+                                <CustomInput 
+                                    id="lastname" 
+                                    label="Last Name" 
+                                    value={lastname} 
+                                    onChange={handleInputChange(setLastname)} 
+                                    error={error} 
                                 />
                             </div>
                         </div>
-                        
-                        {/* Additional form fields (e.g., Email and Mobile No.) */}
+                        <div className="flex flex-wrap -mx-3 mb-6">
+                            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                                <CustomInput 
+                                    id="email" 
+                                    label="Email" 
+                                    value={email} 
+                                    onChange={handleInputChange(setEmail)} 
+                                    error={error} 
+                                />
+                            </div>
+                            <div className="w-full md:w-1/2 px-3">
+                                <CustomInput 
+                                    
+                                    id="mobile" 
+                                    label="Mobile No" 
+                                    value={mobile} 
+                                    onChange={handleInputChange(setMobile)} 
+                                    error={error} 
+                                />
+                            </div>
+                        </div>
+                        <button 
+                        onClick={handleVehicleType} 
+                        className=" -z-10 bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none focus:ring-blue-300 
+                        font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600
+                        dark:hover:bg-blue-700 dark:focus:ring-blue-800" 
+                        type="button"
+                    >
+                        Vechile Type 
+                        <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1L5 5L9 1" />
+                        </svg>
+                    </button>
+
+                    {/* Dropdown menu */}
+                    {vehicleType && (
+                        <div id="vehicleType" className="z-20 bg-white divide-y divide-gray-100 rounded-lg shadow w-1/2 dark:bg-gray-700">
+                            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                                <li>
+                                    <h3 className="block px-4 py-2 hover:bg-blue-300 dark:hover:bg-gray-600 dark:hover:text-white">Used</h3>
+                                </li>
+                                <li>
+                                    <h3 className="block px-4 py-2 hover:bg-blue-300 dark:hover:bg-gray-600 dark:hover:text-white">New</h3>
+                                </li>
+                                
+                            </ul>
+                        </div>
+                    )}
+                    <select name="cars" id="cars">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                    </select>
+  
                     </form>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
